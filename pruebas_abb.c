f2b0b91e6377be1e644223c235c57e12c1a157ff
ariana-salese <71737227+ariana-salese@users.noreply.github.com>
2020-12-01 12:44:34 -0300

    Add files via upload

---
+++ pruebas_abb.c
@@ -0,0 +1,21 @@
+#include "abb.h"
+#include "testing.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// static void prueba_...(void) {
+
+// }
+
+void pruebas_abb_estudiante() {
+    
+}
+
+#ifndef CORRECTOR
+
+int main(void) {
+    pruebas_abb_estudiante();
+    return failure_count() > 0;
+}
+
+#endif
